db9ccabdb6eda74cfbb1117c2573bf797a90699a
samadal <samadal@samadal-PC>
2015-05-12 19:41:43 +0900

    launching 7:41

--- jse/src/cmm03/flow/No07_Kaup.java
+++ jse/src/cmm03/flow/No07_Kaup.java
@@ -3,12 +3,11 @@ package cmm03.flow;
 import java.util.Scanner;
 
 public class No07_Kaup {
-	
+
 	double height;
 	double weight;
 	int idx;
 	Scanner scanner = new Scanner(System.in);
-
 
 	No07_Kaup() {
 		this(0, 0);
@@ -17,7 +16,7 @@ public class No07_Kaup {
 	public No07_Kaup(double i, double j) {
 		this.height = i;
 		this.weight = j;
-		this.idx=(int) (weight/((Math.pow(height, 2))*10000)) ; 
+		this.idx = (int) (weight / ((Math.pow(height, 2)) * 10000));
 	}
 
 	public double getHeight() {
@@ -35,46 +34,50 @@ public class No07_Kaup {
 	public void setWeight(double weight) {
 		this.weight = scanner.nextDouble();
 	}
-	
-	String getMsg(){
-		String msg ="";
-		if(idx>30){msg ="비만";}
-		else if(idx >24){msg="과체중";}
-		else if(idx >20){msg="정상";}
-		else if(idx >15){msg="저체중";}
-		else if(idx >13){msg="마름";}
-		else if(idx >10){msg="영양실조";}
-	    else { msg ="소모증";}
-		return msg ; 
+
+	String getMsg() {
+		String msg = "";
+		if (idx > 30) {
+			msg = "비만";
+		} else if (idx > 24) {
+			msg = "과체중";
+		} else if (idx > 20) {
+			msg = "정상";
+		} else if (idx > 15) {
+			msg = "저체중";
+		} else if (idx > 13) {
+			msg = "마름";
+		} else if (idx > 10) {
+			msg = "영양실조";
+		} else {
+			msg = "소모증";
+		}
+		return msg;
 	}
-	
+
 	@Override
 	public String toString() {
-		return  " 카우프지수 [키 =" + height +"cm , 몸무게=" +weight + "kg 카우프지수= " + getMsg()+"]" ;	}
-	
+		return " 카우프지수 [키 =" + height + "cm , 몸무게=" + weight + "kg 카우프지수= "
+				+ getMsg() + "]";
+	}
 
+	public static void main(String[] args) {
 
-public static void main (String[] args){
-	
-	Scanner scanner = new Scanner(System.in);
-	System.out.println(" 키를 입력하세요 : ");
-	double height = scanner.nextDouble();
-	System.out.println(" 몸무게를 입력하세요 : ");
-	double weight  = scanner.nextDouble();
-	
-	
-	No07_Kaup kaup =new No07_Kaup(height, weight) ; 
-	
-	System.out.print(kaup.toString());
-	
-	
-	System.out.println("완료");
-	scanner.close() ; 
-	
-	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(" 키를 입력하세요 : ");
+		double height = scanner.nextDouble();
+		System.out.println(" 몸무게를 입력하세요 : ");
+		double weight = scanner.nextDouble();
+		
+
+		No07_Kaup kaup = new No07_Kaup(height, weight);
+		System.out.print(kaup.toString());
+		System.out.println("완료");
+		scanner.close();
+		
+		//  static영역에서는 가급적 변수 선언 하지 마라!!
+		//  scanner 의 경우 메인에서 구현하게 하라!! 
+		//  메인에 있는 걸 가급적 객체화하라!!
+
+	}
 }
-}
-
-
-
-
