7242eefdbc5ac254a1e5fbd249a8f39c6e119afc
samadal <samadal@samadal-PC>
2015-05-12 21:50:52 +0900

    launching 09:50

--- jse/src/cmm03/flow/No03_SwitchDemo.java
+++ jse/src/cmm03/flow/No03_SwitchDemo.java
@@ -5,54 +5,50 @@ import java.util.Scanner;
 public class No03_SwitchDemo {
 
 	public static void main(String[] args) {
-		
-		int avg ; 
+
+		System.out.println(" 평균접수입력 : ");
 		Scanner scanner = new Scanner(System.in);
-		avg = scanner.nextInt();
-		
-		
-		/*
+		int avg = scanner.nextInt();
+		int val = 0;
+		int avg2= avg ; 
 
 		do {
-			System.out.println(" 평균접수입력 : ");
-			avg = scanner.nextInt();
-		}while(avg >= 100 || avg <0);
-			String result = "";
-        */
-		
+			avg /= 10;
+			val += 1;
+		} while (avg > 0);
+		System.out.println("입력받은 정수의 자릿수 : " + val);
+		System.out.println("");
+
 		/*
-		if (avg >= 90) {
-			result = "우수합격";
-		} else if (avg >=80) {
-			result = "일반합격";
-		} else {
-			result = "재시험";
-		}
-	 */
-		
-		
-		//삼항연산자 test
-		//result =  avg >=90 ?  "최우수합격" : (avg >= 80 ? "일반합격" :"불합격");
-		//System.out.print(result);
-		
-	
-		String grade ="";
-		
-		switch (avg) {
-		case 9  :
-			grade ="우수합격";
+		 * do { System.out.println(" 평균접수입력 : "); avg = scanner.nextInt();
+		 * }while(avg >= 100 || avg <0); String result = "";
+		 */
+
+		/*
+		 * if (avg >= 90) { result = "우수합격"; } else if (avg >=80) { result =
+		 * "일반합격"; } else { result = "재시험"; }
+		 */
+
+		// 삼항연산자 test
+		// result = avg >=90 ? "최우수합격" : (avg >= 80 ? "일반합격" :"불합격");
+		// System.out.print(result);
+
+		String grade = "";
+
+		switch (avg2/((int)(Math.pow(10,val-1)))){
+		case 10:
+		case 9:
+			grade = "우수합격";
 			break;
-		case 8 :
-			grade ="합격";
+		case 8:
+			grade = "합격";
 			break;
 		default:
-			grade ="불합격";
+			grade = "불합격";
 			break;
 		}
-		System.out.println("입력한 평균은 : " +avg + "점이며 당신은 " + grade + "입니다." );
-			
-		
-		scanner.close();
+
+		System.out.println("입력한 평균은 : " + avg2 + "점이며 당신은 " + grade + "입니다.");
 
 	}
 }
