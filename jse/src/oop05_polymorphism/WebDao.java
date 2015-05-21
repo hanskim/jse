463cbd2e954fdaad896c4df5072ed7936bf4d91a
samadal <samadal@samadal-PC>
2015-05-21 21:08:14 +0900

    launching fianlly2

--- jse/src/oop05_polymorphism/WebDao.java
+++ jse/src/oop05_polymorphism/WebDao.java
@@ -1,5 +1,10 @@
 package oop05_polymorphism;
 
 public interface WebDao {
+	public String getMemers();
+	public String getMemer();
+	public String setMemers();
+	public void deleteMemers();
+	public void updateMemers();
 
 }
