b995463cdfc5376efca5dcc46dcb35e22dc0c82c
samadal <samadal@samadal-PC>
2015-05-21 21:08:14 +0900

    launching fianlly

--- jse/src/oop05_polymorphism/WebDaoImpl.java
+++ jse/src/oop05_polymorphism/WebDaoImpl.java
@@ -1,5 +1,35 @@
 package oop05_polymorphism;
 
-public class WebDaoImpl {
+public class WebDaoImpl implements WebDao{
+
+	@Override
+	public String getMemers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMemer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String setMemers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteMemers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateMemers() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- jse/src/oop05_polymorphism/WebServiceImpl.java
+++ jse/src/oop05_polymorphism/WebServiceImpl.java
@@ -1,6 +1,60 @@
 package oop05_polymorphism;
 
 
-public class WebServiceImpl {
+public class WebServiceImpl implements WebService {
+
+	@Override
+	public String getMemers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMemer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String setMemers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteMemers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateMemers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getAge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String setAge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateAge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteAge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
